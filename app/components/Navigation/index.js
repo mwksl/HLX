9f2a8139e02a8f26d7a9cdb79887a945901c206e
mwksl <matthew@leaguer.io>
2016-07-01 16:12:44 -0500

    Mobile menu updates

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -2,43 +2,90 @@ import React from 'react';
 
 import Logo from './logo.png';
 
-function Navigation() {
-  const navColor = 'rgb(130, 183, 223)';
-  return (
-    <nav className="nav" style={{ background: navColor }}>
-      <div className="nav-left">
-        <a className="nav-item" href="/">
-          <img src={Logo} alt="HealthLX logo" />
-        </a>
-      </div>
+export default class Navigation extends React.Component {
 
-      <span className="nav-toggle">
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+  }
 
-      <div className="nav-right nav-menu">
-        <a className="nav-item" style={{ color: 'white' }} href="/">
-          Home
-        </a>
-        <a className="nav-item" style={{ color: 'white' }} href="http://portal.healthlx.com">
-          Portal
-        </a>
+  handleClick(evt) {
+    evt.preventDefault();
+    console.log(this.state.active);
+    if (this.state.active) {
+      this.state.active = false;
+    } else if (this.state.active === false) {
+      this.state.active = true;
+    }
+  }
 
-        <span className="nav-item">
-          <a className="button is-primary" href="#">
-            <span>Contact</span>
-          </a>
+  render() {
+    const navColor = 'rgb(130, 183, 223)';
+    const navMenu = this.state.active ? (
+      <div>
+        <span className="nav-toggle is-active" onClick={this.handleClick.bind(this)} >
+          <span></span>
+          <span></span>
+          <span></span>
         </span>
-      </div>
-    </nav>
 
-  );
+        <div className="nav-right nav-menu is-active">
+          <a className="nav-item" href="/">
+            Home
+          </a>
+          <a className="nav-item" href="http://portal.healthlx.com">
+            Portal
+          </a>
+
+          <span className="nav-item">
+            <a className="button is-primary" href="#">
+              <span>Contact</span>
+            </a>
+          </span>
+
+        </div>
+      </div>
+      ) : (
+      <div>
+        <span className="nav-toggle" onClick={this.handleClick.bind(this)} >
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div className="nav-right nav-menu">
+          <a className="nav-item" href="/">
+            Home
+          </a>
+          <a className="nav-item" href="http://portal.healthlx.com">
+            Portal
+          </a>
+
+          <span className="nav-item">
+            <a className="button is-primary" href="#">
+              <span>Contact</span>
+            </a>
+          </span>
+
+        </div>
+      </div>
+      );
+    return (
+      <nav className="nav" style={{ background: navColor }}>
+        <div className="nav-left">
+          <a className="nav-item" href="/">
+            <img src={Logo} alt="HealthLX logo" />
+          </a>
+        </div>
+        {navMenu}
+      </nav>
+
+    );
+  }
 }
 
 Navigation.propTypes = {
-  active: React.PropTypes.string,
+  active: React.PropTypes.bool,
 };
-
-export default Navigation;
